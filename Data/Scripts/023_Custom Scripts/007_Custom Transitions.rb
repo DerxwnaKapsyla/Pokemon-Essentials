4265756025f77a2413877bef63bfd78e14cdfdb4
Derxwna Kapsyla <eternaldarknessrebirth@yahoo.com>
2021-02-08 04:16:11 -0500

    Finished up the inagural village tournament
    Added in trainer sprites for a few trainer classes
    * Finished up Kokoro's Vs. Transition
    * Added NPC observers to the Festival Arena
    * Swapped out Kokoro's trainer sprite
    * Bumped up the EXP Scaling to x2 for all battles
    	* Which means the Lucky Egg will just shit out experience but, hey.
    
    February 8th, 2021:
    * Added in the Music Effect for awarding Festival Points
    * Finished up the inaugural tournament section of Stage 1

--- Data/Scripts/023_Custom Scripts/007_Custom Transitions.rb
+++ Data/Scripts/023_Custom Scripts/007_Custom Transitions.rb
@@ -12,17 +12,33 @@ alias CustompbBattleAnimationOverride pbBattleAnimationOverride
 def pbBattleAnimationOverride(viewport,battletype=0,foe=nil)
    if $game_map && $game_switches[104]       # If the switch for Custom Vs. Transitions is on,
      case $game_variables[103]  # check this variable, and depending on the number returned...
-     when 0; pbCommonEvent(6)   # Vs. Yukari
-     when 1; pbCommonEvent(7)   # Vs. Eirin
-     when 2; pbCommonEvent(8)   # Vs. Byakuren
-     when 3; pbCommonEvent(9)   # Vs. Eiki
-     when 4; pbCommonEvent(10)  # Vs. Shinki
-     when 5; pbCommonEvent(12)  # Vs. Derxwna
-     when 6; pbCommonEvent(13)   # Vs. Nue
-     when 7; pbCommonEvent(14)   # Vs. The Collector
-     when 8; pbCommonEvent(15)   # Vs. Renko
-     when 9; pbCommonEvent(16)   # Vs. Maribel
-     when 10; pbCommonEvent(18)  # Vs. Sariel (Puppet)
+     when 0;  pbCommonEvent(9)   # Vs. Kokoro
+	 when 1;  pbCommonEvent(10)  # Vs. Minoriko
+	 when 2;  pbCommonEvent(11)  # Vs. Nitori
+	 when 3;  pbCommonEvent(12)  # Vs. Hatate
+	 when 4;  pbCommonEvent(13)  # Vs. Hina
+	 when 5;  pbCommonEvent(14)  # Vs. Star
+	 when 6;  pbCommonEvent(15)  # Vs. Luna
+	 when 7;  pbCommonEvent(16)  # Vs. Sunny
+	 when 8;  pbCommonEvent(17)  # Vs. Three Fairies
+	 when 9;  pbCommonEvent(18)  # Vs. Medicine
+	 when 10; pbCommonEvent(19)  # Vs. Elly
+	 when 11; pbCommonEvent(20)  # Vs. Yuuka
+	 when 12; pbCommonEvent(21)  # Vs. Keine
+	 when 13; pbCommonEvent(22)  # Vs. Marisa
+	 when 14; pbCommonEvent(23)  # Vs. Youmu
+	 when 15; pbCommonEvent(24)  # Vs. Lyrica
+	 when 16; pbCommonEvent(25)  # Vs. Mystia
+	 when 17; pbCommonEvent(26)  # Vs. Prismrivers
+	 when 18; pbCommonEvent(27)  # Vs. Alice
+	 when 19; pbCommonEvent(28)  # Vs. Sanae
+	 when 20; pbCommonEvent(29)  # Vs. Minoriko and Shizuha
+	 when 21; pbCommonEvent(30)  # Vs. Suika
+	 when 22; pbCommonEvent(31)  # Vs. Sumireko
+	 when 23; pbCommonEvent(32)  # Vs. Gio
+	 when 24; pbCommonEvent(33)  # Vs. Kaliana
+	 when 25; pbCommonEvent(34)  # Vs. Renko and Maribel
+	 when 26; pbCommonEvent(35)  # Vs. Kalypsa
      end
      return true                # Note that the battle animation is done
    end  
