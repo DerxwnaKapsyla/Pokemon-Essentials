f5acb96d5172afb2c65049c7e7d8a88b63a25c32
Derxwna Kapsyla <eternaldarknessrebirth@yahoo.com>
2025-10-17 03:27:17 -0700

    Changes and Additions (10/16/2025):
      - In the Touhoumon intro, Advent should be [Ad]vent
      - Mud Slap is hitting both targets, check the function code
      - Check light screen description
      - Should have made it so Flash Fire is correctly checking for Pyro
      - Also made it sop Sap Sipper, Water Absorb, Lightning Rod, etc now check for their respective Puppet types.
      - Hecatia has wrong abilities
      - Add Move Tutor NPC to Hub and all episodes
      - Removed errant debug event
      - Bind is using wrong main animation
      - Added some AI checks that were missing
    
    TMoM Notes:
      - Slow down Intro a bit more.
      - Add investigate indicator to Fishing Rod box
    
    TFoC Notes:
      - Slow down Intro a bit more.
    
    TKoL Notes:
      - Slow down Intro a bit more.

--- Plugins/[002] Touhoumon DevKit/011_Battle/007_Other battle code/008_Battle_AbilityEffects.rb
+++ Plugins/[002] Touhoumon DevKit/011_Battle/007_Other battle code/008_Battle_AbilityEffects.rb
@@ -27,8 +27,7 @@ Battle::AbilityEffects::PriorityChange.add(:FLOWOFTIME,
 Battle::AbilityEffects::MoveImmunity.add(:FLASHFIRE,
   proc { |ability, user, target, move, type, battle, show_message|
     next false if user.index == target.index
-    next false if type != :FIRE
-    next false if type != :FIRE18
+    next false if type != :FIRE || type != :FIRE18
     if show_message
       battle.pbShowAbilitySplash(target)
       if !target.effects[PBEffects::FlashFire]
